c4a266620e4358aa6e9f31ea6a6b34f7f9a2f361
buddha <madhavikandregula99@gmail.com>
2026-03-14 01:21:32 -0400

    Week2 assignment

---
+++ Wee2_Madhavi_assignments/Using_Pandas.py
@@ -0,0 +1,8 @@
+# pandas are used create tables called dataframes.
+# Matplotlib is artist of python it creates graphs and charts.
+import pandas as pd
+import matplotlib.pyplot as plt
+
+print("Interested learning pandas and matplotlib")
+print("pandas version : ". pd.__version__)
+print("matplotlib version : ", plt.__version__)
